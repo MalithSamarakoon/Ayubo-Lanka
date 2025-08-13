bf676a7116c910709ea153666f419709c702dca7
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-08-13 10:16:37 -0700

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext.jsx";
-import AppContextProvider from "./Context/AppContext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AppContextProvider>
+   
         <ShopContextProvider>
           <App />
         </ShopContextProvider>
-      </AppContextProvider>
+
     </BrowserRouter>
   </StrictMode>
 );
