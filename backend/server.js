d1c8633d9b8aafed49d1f9c479c235fbb25acc16
Oshan <it23533882@my.sliit.lk>
2025-08-15 08:24:16 +0530

    creating routes sign up

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import 'dotenv/config'; 
-import connectDB from './lib/db.js';
+import express from "express";
+import "dotenv/config";
+import connectDB from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
-const app = express(); 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 export default app;
