075da99345604e8f880dcd787079922355866617
Oshan <it23533882@my.sliit.lk>
2025-08-16 16:12:31 +0530

    feat:verify tokens

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
