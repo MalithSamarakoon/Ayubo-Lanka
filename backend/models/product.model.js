6804ee0b3328319f7c2be29cf0209bf5f9a6fde1
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-08-16 13:44:22 +0530

    Created product.model.js

---
+++ backend/models/product.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category"
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    minimumStock: {
+        type: Number,
+        required: true
+    },
+    lastRestockedAt: {
+        type: Date
+    },
+    image: {
+        type: String,
+        required: [true, "Image is required."]
+    }
+}, {timestamps: true});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
