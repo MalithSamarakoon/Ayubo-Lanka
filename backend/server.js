68908ec9c24612960afad60ce6cd08c8865291ec
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-08-18 17:49:41 +0530

    Deleted user authorization related files and modified db.js, server.js files.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
-
 import express from "express";
 import "dotenv/config";
-//import cookieParser from "cookie-parser";
-import connectDB from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
+import connectDB from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,9 +8,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
 
