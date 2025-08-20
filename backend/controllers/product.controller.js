1d98ff2b1539e6d1631cd7dba2538e8edff38420
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-08-20 12:29:45 +0530

    Implemented getAllProducts controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -25,3 +25,16 @@ export const createProduct = async (req, res) => {
         
     }
 }
+
+export const getAllProducts = async (req, res) => {
+      try {
+        const products = await Product.find();
+        res.status(200).json({
+            message: "Products retrieved successfully",
+            products
+        });
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createProduct } from '../controllers/product.controller.js';
+import { createProduct, getAllProducts } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.post('/', createProduct);
+router.get('/allproducts', getAllProducts)
 
 export default router;
