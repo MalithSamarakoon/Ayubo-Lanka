b1e028af5817d88d1458d55200ceadb3bf7e5644
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-08-31 00:14:08 +0530

    Installed zustand and axios

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -22,6 +22,19 @@ const CreateProductForm = () => {
     console.log(newProduct);
   }
 
+  const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+
+			reader.onloadend = () => {
+				setNewProduct({ ...newProduct, image: reader.result });
+			};
+
+			reader.readAsDataURL(file); // base64
+		}
+	};
+
 
   return (
     <>
@@ -107,7 +120,7 @@ const CreateProductForm = () => {
 				
 
 				<div className='mt-1 flex items-center'>
-					<input type='file' id='image' className='sr-only' accept='image/*'  />
+					<input type='file' id='image' className='sr-only' accept='image/*' onChange={handleImageChange} />
 					<label
 						htmlFor='image'
 						className='cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500'
