fba328979d5fc6a63d0019637084998da97ee8ba
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-08-30 10:10:19 +0530

    Started implementing products dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
+import React from 'react'
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductDashboard from './pages/ProductDashboard'
 
 function App() {
   return (
     <div>
       <Routes>
-           <Route path='/' element={<HomePage />}/>
+           <Route path='/product-dashboard' element={<ProductDashboard />}/>
       </Routes>
     </div>
   )
