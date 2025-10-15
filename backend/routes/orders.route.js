c1ba7bccf2f56ec88ad3ca37d2500fe708390b68
MalithSamarakoon <164049919+MalithSamarakoon@users.noreply.github.com>
2025-10-15 06:45:46 +0530

    removed unneccesary comments from orders.route.js

--- backend/routes/orders.route.js
+++ backend/routes/orders.route.js
@@ -11,9 +11,9 @@ import {
 
 const orderRouter = express.Router();
 
-// ✅ Setup multer for slip uploads
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/receipts/"), // folder for bank slips
+  destination: (req, file, cb) => cb(null, "uploads/receipts/"), 
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + file.originalname;
     cb(null, uniqueName);
@@ -21,17 +21,17 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// ✅ CRUD routes
+
 orderRouter.get("/", listOrders);
 orderRouter.get("/:id", getOrder);
 
-// ⚙️ Use upload.single("slip") for create and update to handle file uploads
+
 orderRouter.post("/", upload.single("slip"), createOrder);
 orderRouter.patch("/:id", upload.single("slip"), updateOrder);
 
 orderRouter.delete("/:id", deleteOrder);
 
-// ✅ Optional: update order status only
+
 orderRouter.patch("/:id/status", updateStatus);
 
 export default orderRouter;
